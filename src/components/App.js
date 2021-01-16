e0c3cdf902967c67cdbdce5fe6f89638238d2fda
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2021-01-16 15:26:19 +0100

    Added fetch method in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,15 @@ import './App.css';
 class App extends Component {
 
   state = {
-    value: ""
+    value: "",
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    pressure: "",
+    wind: "",
+    err: ""
   }
 
   handleInputChange = (e) => {
@@ -15,10 +23,28 @@ class App extends Component {
     })
   }
 
+  handleCitySubmit = (e) => {
+    e.preventDefault();
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=714066faa28362cc66e7cc0474695d1f`;
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response
+        }
+        throw Error("Wystąpił błąd")
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
-        <Form value={this.state.value} change={this.handleInputChange} />
+        <Form
+          value={this.state.value}
+          change={this.handleInputChange}
+          submit={this.handleCitySubmit} />
         <Result />
       </div>
     );
